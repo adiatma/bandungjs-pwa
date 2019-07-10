12f7db541df4fd09149b3c38d2d8858da909d279
Dwi Agustianto <dwi.agustianto@ralali.com>
2019-07-10 11:36:13 +0700

    Reset participants

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,11 @@
 
   import Home from '@components/home/Home.svelte'
   import Canvas from '@components/canvas/Canvas.svelte'
+  import Reset from '@components/reset/Reset.svelte'
 </script>
 
 <Router>
   <Route path="/" component={Home} />
   <Route path="/game" component={Canvas} />
+  <Route path="/reset" component={Reset} />
 </Router>
